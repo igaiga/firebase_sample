d494b171a558a43f1e1d8a201cfa88698d96b26d
Kuniaki IGARASHI <igaiga@gmail.com>
2014-10-11 18:10:00 +0900

    mofidy sample

--- firebase_sample.rb
+++ firebase_sample.rb
@@ -3,7 +3,7 @@ url = "https://igaiga-sample.firebaseio.com/"
 firebase = Firebase::Client.new(url)
 
 storage_name = "box1" # 自由に命名可能
-h = {apple: 200, grape: 220} # 保存するHash
+h = {apple: 100, grape: 120} # 保存するHash
 
 # Save
 firebase.set(storage_name, h)
